2f360ebe5d12b852f5d38c85b8a274bc731282dd
navedrizvi <rizvi.naved1997@gmail.com>
2020-05-28 23:20:22 -0400

    Added meta code for visitor interace definitions

--- expr.py
+++ expr.py
@@ -1,22 +1,48 @@
-from abc import ABC
+from token_ import Token
+from abc import ABCMeta, abstractmethod
+
 
 class Expr:
-      class Binary(expr):
-               def __init__(self, left, operator, right):
-                   self.left = left
-                   self.operator = operator
-                   self.right = right
+    pass
 
-      class Grouping(expr):
-               def __init__(self, expression):
-                   self.expression = expression
 
-      class Literal(expr):
-               def __init__(self, value):
-                   self.value = value
+class Binary(Expr):
+    def __init__(self, left: Expr, operator: Token, right: Expr):
+        self.left = left
+        self.operator = operator
+        self.right = right
 
-      class Unary(expr):
-               def __init__(self, operator, right):
-                   self.operator = operator
-                   self.right = right
+
+class Grouping(Expr):
+    def __init__(self, expression: Expr):
+        self.expression = expression
+
+
+class Literal(Expr):
+    def __init__(self, value: object):
+        self.value = value
+
+
+class Unary(Expr):
+    def __init__(self, operator: Token, right: Expr):
+        self.operator = operator
+        self.right = right
+
+
+class IVisitor(metaclass=ABCMeta):
+    @abstractmethod
+    def visit_binary_expr(self, expr: Binary):
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_grouping_expr(self, expr: Grouping):
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_literal_expr(self, expr: Literal):
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_unary_expr(self, expr: Unary):
+        raise NotImplementedError
 
--- generate_ast.py
+++ generate_ast.py
@@ -1,57 +1,62 @@
-# from abc import ABC, abstractmethod
 from token_ import Token
 import sys
-
-
-class Expr():
-    def __init__(self, exprtype):
-        if exprtype == 'Binary':
-            self.binaryexpr = Binary()
-
-    class Binary():
-        def __init__(self, left: super, opertor: Token, right: super):
-            self.left = left
-            self.operator = opertor
-            self.right = right
 
 
 def define_ast(output_dir, base_name, types):
     path = output_dir + "/" + base_name + ".py"
     with open(path, "w+", encoding='utf-8') as writerfp:
         writerfp.writelines([
-            'from abc import ABC\n\n',
+            'from token_ import Token\n',
+            'from abc import ABCMeta, abstractmethod\n\n',
             "class {0}:\n".format(base_name.title()),
+            "    pass\n",
         ])
+        class_names = []
         for type_ in types:
             data = type_.split(":")
-            class_name, class_attributes = data[0].strip(), data[1].strip()
-            define_type(writerfp, base_name, class_name, class_attributes)
+            class_name, raw_class_attributes = data[0].strip(), data[1].strip()
+            class_names.append(class_name)
+            class_attributes = _parse_attributes(raw_class_attributes)
+            _define_type(writerfp, base_name, class_name, class_attributes)
+        _define_visitor_interface(writerfp, base_name, class_names)
 
 
-def define_type(writerfp, base_name, class_name, class_attributes):
+def _define_type(writerfp, base_name: str, class_name: str,
+                 class_attributes: [(str, str)]):
     ''' Declares each field in class body '''
-
-    class_attributes = _get_attributes_without_type(class_attributes)
-    as_string = ', '.join(class_attributes)
+    as_string = ', '.join(
+        [f"{attribute[1]}: {attribute[0]}" for attribute in class_attributes])
     lines = [
         #Constructor
-        f'      class {class_name}({base_name}):\n',
-        f'               def __init__(self, {as_string}):\n',
+        f'class {class_name}({base_name.title()}):\n',
+        f'    def __init__(self, {as_string}):\n',
     ]
-
     for attribute in class_attributes:
-        lines.append(f'                   self.{attribute} = {attribute}\n', )
+        lines.append(' ' * 8 + f'self.{attribute[1]} = {attribute[1]}\n', )
     lines.append('\n')
     writerfp.writelines(lines)
 
 
-def _get_attributes_without_type(st) -> [str]:
+def _define_visitor_interface(writerfp, base_name: str, class_names: [str]):
+    lines = [
+        'class IVisitor(metaclass = ABCMeta):\n',
+    ]
+    for type_name in class_names:
+        #Signatures for visitor methods
+        lines.append(
+            f"{' '*4}@abstractmethod\n{' '*4}def visit_{type_name.lower()}_{base_name.lower()}(self, {base_name.lower()}: {type_name}):\n{' '*8}raise NotImplementedError\n\n"
+        )
+    lines.append('\n')
+    writerfp.writelines(lines)
+
+
+def _parse_attributes(st) -> [(str, str)]:
     ''' >>> 'Type1 v1, Type2 v2'
-        ['v1', 'v2'] 
+        [('Type1', 'v1'), ('Type2', 'v2')] 
     '''
     singles = st.split(',')
     singles = list(map(str.strip, singles))
-    return [single.split(' ')[1] for single in singles]
+    return [(single.split(' ')[0], single.split(' ')[1]) for single in singles]
 
 
 def main():
@@ -60,11 +65,13 @@ def main():
         print("Usage: python3 ast.py <output directory>")
         sys.exit(64)
     out_dir = sys.argv[1]
-    define_ast(out_dir, "expr", [
+    #used for scripting the syntax tree classes
+    expr_params = [
         "Binary: Expr left, Token operator, Expr right",
         "Grouping: Expr expression", "Literal: object value",
         "Unary: Token operator, Expr right"
-    ])
+    ]
+    define_ast(out_dir, "expr", expr_params)
 
 
 if __name__ == "__main__":
